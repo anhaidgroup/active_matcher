23e1e6ed1051b6f4719b78021c05bb5964a0959a
Derek Paulsen <dpaulsen@informatica.com>
2023-07-21 16:49:07 -0500

    add code, not tested yet

--- active_matcher/labeler.py
+++ active_matcher/labeler.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod, ABC
+import time
 
 
 class Labeler(ABC):
@@ -22,3 +23,14 @@ class GoldLabeler(Labeler):
     def __call__(self, id1, id2):
         return 1.0 if (id1, id2) in self._gold else 0.0
 
+class DelayedGoldLabeler(Labeler):
+
+    def __init__(self, gold, delay_secs):
+        self._gold = gold
+        # the number of seconds that the labeler waits until it outputs the label
+        # this is used to simulate human labeling
+        self._delay_secs = delay_secs
+
+    def __call__(self, id1, id2):
+        time.sleep(self._delay_secs)
+        return 1.0 if (id1, id2) in self._gold else 0.0
